429a054b8d3c340229104e998c21b0f53b05b1a0
okauefonseca <124893260+Fonseca-Dev@users.noreply.github.com>
2025-05-27 11:06:20 -0300

    Delete Aula3LoginApp directory

--- Aula3LoginApp/Valores.java
+++
@@ -1,8 +0,0 @@
-package com.example.aula3loginapp;
-
-public class Valores {
-    public static final String arquivo = "Aula6";
-
-    public static final String login = "aluno";
-    public static final String senha = "123456";
-}
